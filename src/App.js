764fbca16c5cecf19e92d875dd4b3d78630c1005
shubham <shubham25122001@gmail.com>
2023-06-11 07:36:54 +0530

    uploded image insert qr code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import html2canvas from "html2canvas";
 import { QRCodeCanvas } from "qrcode.react";
 import React, { useState } from "react";
+import Dropzone from 'react-dropzone';
 import { AiFillCopy, AiOutlineDownload } from "react-icons/ai";
 
 function App() {
   const [qr, setqr] = useState("");
   const [url, seturl] = useState("");
+  const [file, setFile] = useState(null);
   
   const QrCodeDownload = async () => {
     const canvas = await (
@@ -26,19 +28,40 @@ function App() {
   const QrCodeCopy = () => {
     navigator.clipboard.writeText(qr);
   };
+
+  const handleDrop = (acceptedFiles) => {
+    if (acceptedFiles && acceptedFiles.length > 0) {
+      const uploadedFile = acceptedFiles[0];
+      setFile(URL.createObjectURL(uploadedFile));
+    }
+  };
+
   return (
     <div className="container mx-auto w-[320px]">
-      <div class="mb-4">
+      <div className="mb-4">
         <p className="text-2xl">Generate QrCode</p>
       </div>
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm  mb-2">Write something</label>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm  mb-2">Write something</label>
         <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           type="text"
           onChange={(e) => seturl(e.target.value)}
         />
       </div>
+
+      <Dropzone onDrop={handleDrop}>
+        {({ getRootProps, getInputProps }) => (
+          <div {...getRootProps()} className="dropzone">
+            <input {...getInputProps()} />
+            {file ? (
+              <img src={file} alt="Uploaded" className="uploaded-image" />
+            ) : (
+              <p>Drag 'n' drop an image here, or click to select files</p>
+            )}
+          </div>
+        )}
+      </Dropzone>
 
       <div id="canvas" className="border p-2 relative">
         <QRCodeCanvas
@@ -49,7 +72,8 @@ function App() {
           level={"H"}
           includeMargin={false}
           imageSettings={{
-            src: "/shubham.png",
+            // src: "/shubham.png",
+            src: file,
             x: undefined,
             y: undefined,
             height: 40,
@@ -62,7 +86,7 @@ function App() {
       <div className="flex w-[300px] mt-4 p-4 space-x-2 items-center justify-center">
         <button
           onClick={() => QrCodeDownload()}
-          class="flex items-center justify-between bg-transparent hover:bg-[#0a75ad] text-[#0a75ad] font-semibold hover:text-white py-2 px-4 border border-[#0a75ad] hover:border-transparent rounded"
+          className="flex items-center justify-between bg-transparent hover:bg-[#0a75ad] text-[#0a75ad] font-semibold hover:text-white py-2 px-4 border border-[#0a75ad] hover:border-transparent rounded"
         >
           <AiOutlineDownload />
           Download
@@ -70,7 +94,7 @@ function App() {
 
         <button
           onClick={() => QrCodeCopy()}
-          class="flex items-center  justify-between bg-transparent hover:bg-[#0a75ad] text-[#0a75ad] font-semibold hover:text-white py-2 px-4 border border-[#0a75ad] hover:border-transparent rounded"
+          className="flex items-center  justify-between bg-transparent hover:bg-[#0a75ad] text-[#0a75ad] font-semibold hover:text-white py-2 px-4 border border-[#0a75ad] hover:border-transparent rounded"
         >
           <AiFillCopy />
           Copy
