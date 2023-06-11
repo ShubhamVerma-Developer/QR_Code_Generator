d0cc5537c4f27a5f7c9319e81e2a5849a7513861
shubham <shubham25122001@gmail.com>
2023-06-11 22:19:48 +0530

    qr code app work

--- src/App.js
+++ src/App.js
@@ -32,15 +32,18 @@ function App() {
   };
 
   return (
-    <div className="grid-container">
-    <div className="App">
-      <h1>QR Code Generator</h1>
+    <>
+    <div className="container text-center">
+    <h1>QR Code Generator</h1>
+    <div className="grid-container"> 
+    <div className="full-width">
       <textarea
         value={text}
         onChange={(e) => setText(e.target.value)}
         placeholder="Enter text"
       ></textarea>
-
+    </div>
+    <div className="grid-item">
       <Dropzone onDrop={handleDrop}>
         {({ getRootProps, getInputProps }) => (
           <div {...getRootProps()} className="dropzone">
@@ -53,14 +56,14 @@ function App() {
           </div>
         )}
       </Dropzone>
-
+      </div>
+      <div className="grid-item">
       <div id="canvas" className="qr-code-container">
-
          <QRCodeCanvas
           value={text}
           size={300}
           bgColor={"#ffffff"}
-          fgColor={"#0a75ad"}
+          fgColor={"black"}
           level={"H"}
           includeMargin={false}
           imageSettings={{
@@ -68,32 +71,32 @@ function App() {
             src: file,
             x: undefined,
             y: undefined,
-            height: 60,
-            width: 60,
+            height: 70,
+            width: 70,
             excavate: true,
             style: {
               borderRadius: 30,
             },
           }}
-
-
-
         />
-      </div>
-      <div className="">
+       </div>
+        
+      
         <button
           onClick={() => QrCodeDownload()}
-          className="btn"
+          className="btn btn-primary center full-button"
         >
-          <i className="fa fa-download mr-3"></i>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cloud-arrow-down-fill" viewBox="0 0 16 16">
+  <path d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 6.854-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z"/>
+</svg>
           Download
         </button>
+        </div>
       </div>
+      
+   
     </div>
-    </div>
-    
+    </>
   );
-  
 }
-
 export default App;
